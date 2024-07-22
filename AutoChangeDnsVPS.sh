7808d6d74d0f34273e55b5c69340caa9ded49b20
PR Aiman <58582776+praiman99@users.noreply.github.com>
2024-07-22 12:30:26 +0800

    Update AutoChangeDnsVPS.sh

--- AutoChangeDnsVPS.sh
+++ AutoChangeDnsVPS.sh
@@ -12,6 +12,7 @@ wget -O Dns "https://github.com/praiman99/AutoChangeDnsVPS/raw/Beginner/Dns.sh";
 echo "10 0 * * * root bash Dns" >> /etc/crontab
 
 # Add Auto Enable Warp At Crontab
+cd /usr/bin
 wget -O warp "git.io/warp.sh"; chmod +x warp
 echo "15 0 * * * root bash warp wg4" >> /etc/crontab
 
