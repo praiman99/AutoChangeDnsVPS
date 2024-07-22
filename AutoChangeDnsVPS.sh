4e3362190314ee42072cf6fb1ae5cf30f382f797
PR Aiman <58582776+praiman99@users.noreply.github.com>
2024-07-22 13:13:44 +0800

    Update AutoChangeDnsVPS.sh

--- AutoChangeDnsVPS.sh
+++ AutoChangeDnsVPS.sh
@@ -15,10 +15,3 @@ echo "10 0 * * * root bash Dns" >> /etc/crontab
 cd /usr/bin
 wget -O warp "git.io/warp.sh"; chmod +x warp
 echo "15 0 * * * root bash warp wg4" >> /etc/crontab
-
-# Reboot
-echo ""
-echo " Script will reboot "
-echo ""
-sleep 1.0
-reboot
