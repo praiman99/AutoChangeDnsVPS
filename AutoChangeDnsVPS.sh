02d1301ff0101d631b1058d1dc0dec0b7365d1c3
PR Aiman <58582776+praiman99@users.noreply.github.com>
2024-07-22 12:25:25 +0800

    Update AutoChangeDnsVPS.sh

--- AutoChangeDnsVPS.sh
+++ AutoChangeDnsVPS.sh
@@ -4,18 +4,15 @@
 # Copyright©Beginner
 ============================
 
-# Remove old dns
-rm -f /etc/resolv.conf
+# Download file Dns Change
+cd /usr/bin
+wget -O Dns "https://github.com/praiman99/AutoChangeDnsVPS/raw/Beginner/Dns.sh"; chmod +x Dns
 
-# Add new dns
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-
-# Add Auto Change At Crontab
-echo "10 0 * * * root bash AutoChangeDnsVPS" >> /etc/crontab
+# Add Dns Change At Crontab
+echo "10 0 * * * root bash Dns" >> /etc/crontab
 
 # Add Auto Enable warp
-wget -O warp "git.io/warp.sh" chmod +x warp
+wget -O warp "git.io/warp.sh"; chmod +x warp
 echo "15 0 * * * root bash warp wg4" >> /etc/crontab
 
 # Reboot
